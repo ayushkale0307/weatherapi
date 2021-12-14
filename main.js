0b8b5f54a23e0271dc4b1e5b63265f1a3e4d0ddb
ayushkale0307 <91651097+ayushkale0307@users.noreply.github.com>
2021-12-14 23:58:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=66a3953c53e94c57bddaf79e61e1dffc&units=metric",
+        
+      }).done(function(data) {
+          console.log(data)
+          $(".london").html(data.main.temp)
+          
+      });
+})
+$(document).ready(function(){
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather?q=Paris&APPID=66a3953c53e94c57bddaf79e61e1dffc&units=metric",
+      }).done(function(data)  {
+          console.log(data)
+          $(".paris").html(data.main.temp)
+      });
+})
